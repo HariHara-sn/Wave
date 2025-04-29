d5017359f3f0480c3b2784503a5f28434f0f8b02
britto-772004 <michaeljohnbritto772004@gmail.com>
2025-04-29 10:49:06 +0530

    updated teacher drive

---
+++ APP/BACKEND/routes/socialMediaRoutes.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+// const { createPost,editPost,deletePost,viewPost } = require("../controllers/socialMediaController");
+
+// const app = express.Router();
+
+// app.post("/createPost",createPost)
+// app.get("/viewPost",viewPost)
+// app.post("/editPost",editPost)
+// app.post("/deletePost",deletePost)
+
+// module.exports = app;
